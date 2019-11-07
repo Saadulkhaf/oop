3b66b2a8c993dc14d696bb79c76e47ddd7aa37ec
Saadulkhaf <53575848+Saadulkhaf@users.noreply.github.com>
2019-11-07 13:45:06 +0500

    Add files via upload

---
+++ Classes/src/Cat.cpp
@@ -0,0 +1,17 @@
+/*
+ * Cat.cpp
+ *
+ *  Created on: Nov 7, 2019
+ *      Author: Saad Khaf
+ */
+
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void speak() {
+	cout << "Meowww" << endl;
+}
+
+
---
+++ Classes/src/Cat.h
@@ -0,0 +1,15 @@
+/*
+ * Cat.h
+ *
+ *  Created on: Nov 7, 2019
+ *      Author: Saad Khaf
+ */
+
+#ifndef CAT_H_
+#define CAT_H_
+
+void speak();
+
+
+
+#endif /* CAT_H_ */
